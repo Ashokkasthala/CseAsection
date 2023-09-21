a32dc6d58051637dce33575f110b5dacd3f5a7e4
Admin <Admin@DESKTOP-0KTMHPU>
2023-09-21 14:02:24 +0530

    Team lead commit

---
+++ src/com/lbrce/CseAsection/Sample.java
@@ -0,0 +1,19 @@
+package com.lbrce.CseAsection;
+
+public class Sample 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Ashok();
+
+	}
+	public static void Ashok() 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("This is the team Lead:Ashok");
+
+	}
+
+}
